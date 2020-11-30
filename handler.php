ab91e01fd89958b4765c6dfa351b532d33b941d4
lesartcom <66276340+lesartcom@users.noreply.github.com>
2020-11-30 21:08:43 +0100

    Update handler.php

--- handler.php
+++ handler.php
@@ -1,27 +1,28 @@
-<?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-/*
-Tested working with PHP5.4 and above (including PHP 7 )
-
- */
-require_once './vendor/autoload.php';
-
-use FormGuide\Handlx\FormHandler;
-
-
-$pp = new FormHandler();
-
-$validator = $pp->getValidator();
-$validator->fields(['name','email'])->areRequired()->maxLength(50);
-$validator->field('email')->isEmail();
-$validator->field('message')->maxLength(6000);
-
-
-
-
-$pp->sendEmailTo('lakhouaneomar@hotmail.com');
-
-echo $pp->process($_POST);
-Copy
+<?php session_start();
+if(isset($_POST['submit'])) {
+$youremail = 'lakhouaneomar@hotmail.com';
+$fromsubject = 'Contact Form';
+$name = $_POST['name'];
+$mail = $_POST['email'];
+$subject = $_POST['subject']; 
+$message = $_POST['message']; 
+$to = $youremail; 
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type:text/html; charset=UTF-8' . "\r\n";
+$headers .= "From: ".$_POST['name']."<".$_POST['Email'].">\r\n"; 
+$headers .= "Reply-To: ".$_POST["email"]."\r\n";
+$mailsubject = 'Messsage recived for'.$fromsubject.' Contact Page';
+$body = $fromsubject.'
+	Nouveau message  '.$name.'
+	 E-mail: '.$mail.'
+	 Subject: '.$subject.'
+	
+	 Message: 
+	 '.$message.'	
+	|---------END MESSAGE----------|'; 
+echo "Thank you fo your feedback. We will contact you shortly if needed."; 
+								mail($to, $subject, $body,$headers);
+ } else { 
+echo "You must write a message."; 
+}
+?> 
